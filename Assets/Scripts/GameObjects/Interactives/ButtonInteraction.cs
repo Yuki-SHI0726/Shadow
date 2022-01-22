a51b70c1b483a7ff10ae94c975b490639ac63f55
Yuki-SHI0726 <szx0726@gmail.com>
2022-01-22 00:15:49 -0800

    Updated button interactions

--- Assets/Scripts/GameObjects/Interactives/ButtonInteraction.cs
+++ Assets/Scripts/GameObjects/Interactives/ButtonInteraction.cs
@@ -6,15 +6,9 @@ using DG.Tweening;
 
 public class ButtonInteraction : Interactive
 {
-<<<<<<< HEAD
-    [SerializeField] private bool m_pressed = false;
-    [SerializeField] private Object m_door = null;
-=======
     [SerializeField] private bool Pressed = false;
     [SerializeField] public UnityEvent OnButtonPressed;
     [SerializeField] public UnityEvent OnButtonRealese;
-
->>>>>>> 245f07ec45dc29d02a90f86b8786fd2b1344eb27
 
     private void Start()
     {
@@ -23,22 +17,13 @@ public class ButtonInteraction : Interactive
 
     public override void OnInteract()
     {
-<<<<<<< HEAD
-        m_pressed = true;
-    }
-
-    public override void OnExitInteract()
-    {
-        m_pressed = false;
-=======
         Pressed = true;
         OnButtonPressed.Invoke();
     }
 
     public override void OnExitInteract()
-    { 
+    {
         Pressed = false;
         OnButtonRealese.Invoke();
->>>>>>> 245f07ec45dc29d02a90f86b8786fd2b1344eb27
     }
 }
