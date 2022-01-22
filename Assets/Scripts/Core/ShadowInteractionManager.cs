25bea3f370c53f3fa83f7594642784bfd6f82eb8
Rescarf <459885351@qq.com>
2022-01-22 17:35:16 +0800

    Correct BackRegion.prefab, shadow with interaction

--- Assets/Scripts/Core/ShadowInteractionManager.cs
+++ Assets/Scripts/Core/ShadowInteractionManager.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Manager of the Interaction
-/// </summary>
-public class InteractionManager : MonoBehaviour
+public class ShadowInteractionManager : MonoBehaviour
 {
-    [SerializeField] private  GameObject PlayerObject;
+    [SerializeField] private GameObject ShadowObject;
+    [SerializeField] private ShadowController ShadowController;
+    [SerializeField] private GameObject PlayerObject;
     [SerializeField] private Controller PlayerController;
 
     void Start()
     {
         PlayerObject = GameObject.FindGameObjectWithTag("Player");
-        PlayerController = PlayerObject.GetComponent<Controller>();                          
+        PlayerController = PlayerObject.GetComponent<Controller>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -34,7 +33,7 @@ public class InteractionManager : MonoBehaviour
             MovablePlane Plane = collision.gameObject.GetComponent<MovablePlane>();
             if (Plane != null)
             {
-                PlayerController.ExtensionVelocity = Plane.GetVelocity();
+                Debug.Log("影子也要走");
             }
         }
     }
@@ -51,7 +50,8 @@ public class InteractionManager : MonoBehaviour
         if (collision.tag == PlayerController.MovableTag)
         {
             // Stop following the movale plane
-            PlayerController.ExtensionVelocity = Vector3.zero;          
+            Debug.Log("影子的速度是0");
         }
     }
+
 }
--- Assets/Scripts/GameObjects/Interactives/ButtonInteraction.cs
+++ Assets/Scripts/GameObjects/Interactives/ButtonInteraction.cs
@@ -8,10 +8,14 @@ public class ButtonInteraction : Interactive
 {
     [SerializeField] public UnityEvent OnButtonPressed;
     [SerializeField] public UnityEvent OnButtonRealese;
+    [SerializeField] private LayerMask m_interactionLayer;
+
+    private BoxCollider2D m_boxCollider;
 
     private void Start()
     {
         Type = InteractiveType.kButton;
+        m_boxCollider = GetComponent<BoxCollider2D>();
     }
 
     public override void OnInteract()
@@ -21,6 +25,11 @@ public class ButtonInteraction : Interactive
 
     public override void OnExitInteract()
     {
-        OnButtonRealese.Invoke();
+        //要检测这个按钮上还没有没有InteractionManagerLayer的碰撞
+        Collider2D HitInfo =  Physics2D.OverlapBox(transform.position, m_boxCollider.size, 0, m_interactionLayer);
+        if(HitInfo == null)
+        {
+            OnButtonRealese.Invoke();
+        }
     }
 }
